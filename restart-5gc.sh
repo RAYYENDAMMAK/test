3a0937cce5b61f440734df67f8b7aa1344701e1d
Rayen Dammak <rayendammak12345@gmail.com>
2026-04-20 10:43:01 +0100

    adding scp

---
+++ restart-5gc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+NAMESPACE="open5gs"
+
+echo "Restarting Open5GS Control Plane..."
+kubectl rollout restart deployment/nrf -n $NAMESPACE
+kubectl rollout restart deployment/scp -n $NAMESPACE
+kubectl rollout restart deployment/amf -n $NAMESPACE
+kubectl rollout restart deployment/smf -n $NAMESPACE
+kubectl rollout restart deployment/ausf -n $NAMESPACE
+kubectl rollout restart deployment/udm -n $NAMESPACE
+kubectl rollout restart deployment/udr -n $NAMESPACE
+kubectl rollout restart deployment/pcf -n $NAMESPACE
+kubectl rollout restart deployment/nssf -n $NAMESPACE
+kubectl rollout restart deployment/bsf -n $NAMESPACE
+
+echo "Restarting User Plane (UPF)..."
+kubectl rollout restart deployment/upf -n $NAMESPACE
+
+echo "Restarting MongoDB..."
+# MongoDB is a StatefulSet
+kubectl delete pods -l app=mongodb -n $NAMESPACE
+
+echo "Restarting UERANSIM..."
+kubectl rollout restart deployment/ueransim-ue -n $NAMESPACE
+# gNB is a StatefulSet
+kubectl delete pods -l app=ueransim-gnb -n $NAMESPACE
+
+echo "Waiting for pods to be ready..."
+kubectl get pods -n $NAMESPACE -w
